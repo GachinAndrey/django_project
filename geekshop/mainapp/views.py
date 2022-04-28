2a439986d5f0b5cc30bc6a668dda102ea48d3b0c
GachinAndrey <gachin.andrei@gmail.com>
2022-04-28 09:00:39 +0300

    fixed css operation, lesson 02 is finished

--- geekshop/mainapp/views.py
+++ geekshop/mainapp/views.py
@@ -1,29 +1,41 @@
-from django.urls import reverse
+pipfrom django.urls import reverse
 from django.shortcuts import render
 
 MENU_LINKS = {
-    'index': 'Главная',
-    'products': 'Продукты',
-    'contact': 'Контакты',
+    "index": "Главная",
+    "products": "Продукты",
+    "contact": "Контакты",
 }
 
 
 def main(request):
-    return render(request, "mainapp/index.html", context= {
-        'title': 'Главная',
-        'menu': MENU_LINKS,
-    })
+    return render(
+        request,
+        "mainapp/index.html",
+        context={
+            "title": "Главная",
+            "menu": MENU_LINKS,
+        },
+    )
+
 
 def products(request):
-    return render(request, "mainapp/products.html", context= {
-        'title': 'Продукты',
-        'menu': MENU_LINKS,
-    })
+    return render(
+        request,
+        "mainapp/products.html",
+        context={
+            "title": "Продукты",
+            "menu": MENU_LINKS,
+        },
+    )
 
 
 def contact(request):
-    return render(request, "mainapp/contact.html", context= {
-        'title': 'Контакты',
-        'menu': MENU_LINKS,
-    })
-    
+    return render(
+        request,
+        "mainapp/contact.html",
+        context={
+            "title": "Контакты",
+            "menu": MENU_LINKS,
+        },
+    )
